7df35790fe99e3a41d1a3d5a7810ffa64ea9a261
Brayan Q <baquinonez3@misena.edu.co>
2021-09-04 18:53:24 -0500

    Fin seccion 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,29 +1,15 @@
 import { NgModule } from '@angular/core';
 import {Routes , RouterModule} from '@angular/router'
-import { LoginComponent } from './auth/login/login.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { DasboardComponent } from './pages/dasboard/dasboard.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
+import { AuthRoutingModule } from './auth/auth.routing';
 import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
-import { ProgressComponent } from './pages/progress/progress.component';
+import { PagesRoutingModule } from './pages/pages.routing';
+
 
  const routes : Routes = [
-    {path:'' , 
-    component:PagesComponent,
-    children:[
 
-      {path : 'progress' , component : ProgressComponent},
-      {path : 'grafica1' , component : Grafica1Component},
-      {path : 'dashboard' , component : DasboardComponent},
-      {path:'' ,pathMatch:'full' , redirectTo:'/dashboard'},
-    ]
-  },
-   {path : 'login' , component : LoginComponent},
-   {path : 'register' , component : RegisterComponent},
   
    
- 
+   {path:'' ,pathMatch:'full' , redirectTo:'/dashboard'},
    {path:'**', pathMatch:'full', component:NopagefoundComponent}
 
 
@@ -33,7 +19,9 @@ import { ProgressComponent } from './pages/progress/progress.component';
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes),
+    PagesRoutingModule,
+    AuthRoutingModule
   ],
   exports: [
     RouterModule
